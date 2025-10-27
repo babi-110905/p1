5e32bd2e343e235abdb084eace24967da82c802d
babi-110905 <babi110905@gmail.com>
2025-10-27 23:34:48 +0530

    Add files via upload

---
+++ ToDoList.java
@@ -0,0 +1,11 @@
+public class ToDoList {
+    public static void main(String[] args) {
+        System.out.println("<html><body><h2>My To-Do List</h2>");
+        System.out.println("<ul>");
+        System.out.println("<li>Complete homework</li>");
+        System.out.println("<li>Submit project report</li>");
+        System.out.println("<li>Prepare for exams</li>");
+        System.out.println("</ul>");
+        System.out.println("</body></html>");
+    }
+}
